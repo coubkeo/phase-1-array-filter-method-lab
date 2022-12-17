f6d8f2c45febfceabe820c916c443329aa4730f5
CHRISTINA OUBKEO <COUBKEO@GMAIL.COM>
2022-12-16 21:36:44 -0700

    commit tree cb8a4834070cb86e389da0694b64f9045c5b82d9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, attribute){
+    return drivers.filter( function (drivers) {return drivers.toLowerCase() === attribute.toLowerCase()} )
+}
+
+function fuzzyMatch(drivers, attribute){
+    return drivers.filter(function (drivers) {return drivers.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()})
+}
+
+function matchName(drivers, argument){
+    return drivers.filter(function (drivers){ return drivers.name === argument})
+}
